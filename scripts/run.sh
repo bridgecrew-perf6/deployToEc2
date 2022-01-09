9b319e49c60826d7f521b8a2543c8f34a4b479f0
jackzhen1996 <jackzhen999@gmail.com>
2022-01-08 23:47:37 -0800

    added npm install pm2

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/deployToEc2
 
 echo "Start app for current directory: ${PWD}"
 
-pm2 start npm -- start
+pm2 start npm -- start --name "test_server"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ cd /home/ec2-user/deployToEc2
 echo "Install packages/start app for current directory: ${PWD}"
 
 npm install
-
+npm install pm2 -g
 
 
